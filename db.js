a74c4f981b2c6bea85ef0c85af00e3421c31c726
Felipe Mejias <felipe.mejias50@gmail.com>
2022-06-10 13:55:14 -0300

    preparado para deploy

--- db.js
+++ db.js
@@ -2,18 +2,18 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'Hey000ya';
-const host = '127.0.0.1';
-const port = 5432;
-const database = 'teste16';
-
-const db = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
+const db = new Pool(databaseConfig);
 
 export default db
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express,{json} from "express"
 import cors from 'cors'
+import dotenv from 'dotenv'
 import {authRouter} from './routers/authRouter.js'
 import {urlRouter} from './routers/urlRouter.js'
 import {userRouter} from './routers/userRouter.js'
 
+dotenv.config()
 const app=express()
 app.use(cors())
 app.use(json())
@@ -12,4 +14,5 @@ app.use(authRouter)
 app.use(urlRouter)
 app.use(userRouter)
 
-app.listen(5003)
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server running on port ${port}`))
